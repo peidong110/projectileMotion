f56f12222696ec6de3de92452eda40ab99050533
Dong Pei <silence1198@outlook.com>
2020-03-10 17:59:46 -0400

    input

--- a3/main.swift
+++ a3/main.swift
@@ -16,10 +16,61 @@ func getInput() ->String{
     //This function is meant to get input from user
     
 }
+func correctTypeDouble()->Double{
+    var returnVal:Double = -1
+    let input = readLine()
+    if let input = input {
+        if input == "quit" {
+            print("Quit the programme now")
+        }
+        else{
+    //        print(input) // -> the input will be printed out.
+            let con = Double(input)
+            if(con == nil){
+                print("Nor Quit nor float")
+                returnVal = con ?? -1.0
+            }
+            else{
+                print(con!)
+                returnVal = con ?? -1.0
+            }
+        }
+    }
+    return returnVal
+}
+func correctTypeInt()->Int{
+    var returnVal:Int = -1
+    let input = readLine()
+    if let input = input {
+        if input == "quit" {
+            print("Quit the programme now")
+        }
+        else{
+    //        print(input) // -> the input will be printed out.
+            let con = Int(input)
+            if(con == nil){
+                print("Nor Quit nor float")
+                returnVal = con ?? -1
+            }
+            else{
+                print(con!)
+                returnVal = con ?? -1
+            }
+        }
+    }
+    return returnVal
+}
 
 
 
-print("Please initial height of the projectile above the ground (in meters)")
-var height = getInput()
-if(self.stringValueOfInitialHeight == Int)
+print("Please enter initial height of the projectile above the ground (in meters)")
+var height:Double = correctTypeDouble()
+
+print("Please enter initial angle of release (in degrees)")
+var angle:Double = correctTypeDouble()
+print("Please enter initial velocity of the projectile (in meters/s")
+var velocity:Double = correctTypeDouble()
+print("Please enter number of divisions per second for the simulation to use (e.g. 100)")//integer value fot this
+var performance:Int = correctTypeInt()
+
 
